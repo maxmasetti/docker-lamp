928bdc2dc0f31074a3b6c15f6be61daad0e7bf8b
Max <max>
2018-03-11 21:47:40 +0100

    modificato index.php

--- www/index.php
+++ www/index.php
@@ -1,3 +1,4 @@
+<pre>
 <?php
 
 #phpinfo(); die;
@@ -13,5 +14,18 @@ if (!$db) {
 
 echo "Success: A proper connection to MySQL was made!" . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($db) . PHP_EOL;
+
+$sql = "SHOW VARIABLES";
+
+$table = [];
+$rs = mysqli_query($db, $sql);
+$r = mysqli_fetch_assoc($rs);
+while ($r) {
+  $table[] = $r;
+  $r = mysqli_fetch_assoc($rs);
+}
+
+echo "\nSHOW VARIABLES =>\n";
+echo json_encode($table, JSON_PRETTY_PRINT);
 
 mysqli_close($db);
